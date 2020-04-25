b2c3aef7c4442b1ca92861da953ae2b0422c1a26
韵 观 <guanyun.helo@outlook.com>
2020-04-25 14:56:45 +0000

    feat:init pro

---
+++ 累加合/index.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: helo
+ * @Date: 2020-04-25 14:35:06
+ * @LastEditTime: 2020-04-25 14:56:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /algorithm/累加合/index.js
+ */
+
+ /*
+ *@n 累加
+ */
+ function subAdd(n){
+    let sum = 0; // 运行一次
+    let i = 1; // 运行一次
+    for(i<n;i++;){ //运行n次
+        sum = sum + i; // 运行n次
+    }
+    return sum
+ }
+
+ /*
+ *@n 累加
+ */
+function subAdd(n){
+    let sum = 0; // 运行一次
+    let i = 1; // 运行一次
+    for(i<n;i++;){ //运行n次
+        sum = sum + i; // 运行n次
+    }
+    return sum
+ }
+
+ 
