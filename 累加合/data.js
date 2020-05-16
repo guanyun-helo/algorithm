19ee6a9715d7f124b48e931b47f1abb216bf7c43
helo <helo@helo-guanyun.lan>
2020-05-16 13:46:29 +0800

    fix: 🐛 add twosums

---
+++ 累加合/data.js
@@ -0,0 +1,19 @@
+const testExample = [
+    {
+        nums: [2,7,11,15],
+        target: 9,
+        answer: [0, 1]
+    },
+    {
+        nums: [3,2,4],
+        target: 6,
+        answer: [1,2]
+    },
+    {
+        nums: [3,3],
+        target: 6,
+        answer: [0,1]
+    }
+]
+module.exports = testExample
+// export testExample
--- 累加合/index.js
+++ 累加合/index.js
@@ -1,40 +1,36 @@
 /*
- * @Author: helo
- * @Date: 2020-04-25 14:35:06
- * @LastEditTime: 2020-04-25 15:08:31
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /algorithm/累加合/index.js
+ * @title: 兩數之和
+ * @des: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  */
 
- /*
- *@n 累加
- */
- function subAdd(n){
-    let sum = 0; // 运行一次
-    let i = 1; // 运行一次
-    for(i<n;i++;){ //运行n次
-        sum = sum + i; // 运行n次
-    }
-    return sum
- }
+/*
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
 
- // Tn = O(2n+2)
 
- /*
- *@n 累加
- */
-function subAdd(n){
-    let sum = 0; // 运行一次
-    let i = 1; // 运行一次
-    for(i<=n;i++;){ //运行n次
-        for(let j =1;j<=n;j++){ // 运行n^2次
-            sum = sum + i + j // 运行n^2次
+const example = require('./data')
+/*
+@nums 數組
+@target 目標值
+注意在 map.set(nums[i],i) 後setvalue
+*/
+function twoSum(nums,target){
+    let map = new Map()
+    for(let i in nums){
+        if(map.get(target - nums[i]) !== undefined ){
+            return [Number(map.get(target - nums[i])),Number(i)]
         }
-    }
-    return sum
- }
+        map.set(nums[i],i)
 
- // Tn = O(2 + n + 2n^2)
+    }
+}
+
+example.forEach((item)=>{
+    let res = twoSum(item.nums,item.target)
+})
+
+
 
  
