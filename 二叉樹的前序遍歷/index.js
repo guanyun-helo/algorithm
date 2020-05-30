b557d1d01475c43a54222c5b4fe7b5f83333f0c8
heloguanyun <guanyun.helo@gmail.com>
2020-05-31 00:46:46 +0800

    Gitacora saved 1 changed files

--- 二叉樹的前序遍歷/index.js
+++ 二叉樹的前序遍歷/index.js
@@ -10,3 +10,16 @@ var preorderTraversal = function(root) {
     }
     return res
 };
+
+function preorderTraversal02(root){
+    const stack = [];
+    const res = [];
+    root && stack.push(root) //根節點入棧
+    while(stack.length > 0){
+        let popedEle = stack.pop() // 最後一個節點出戰
+        res.push(popedEle.val)
+        popedEle.right&& stack.push(popedEle.right)
+        popedEle.left && stack.push(popedEle.left)
+    }
+    return res
+}
